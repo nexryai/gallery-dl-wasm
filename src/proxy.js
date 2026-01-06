e8f306213bc3dd57ccf8c6c976c6fa39caa9fb7f
nexryai <61890205+nexryai@users.noreply.github.com>
2026-01-06 06:25:42 +0000

    Fix proxy

--- src/proxy.js
+++ src/proxy.js
@@ -16,7 +16,7 @@ export default class extends WorkerEntrypoint {
         const url = new URL(request.url);
 
         const requestOrigin = request.headers.get("Origin");
-        if (!CORS_ALLOWED_DOMAINS.some(domain => requestOrigin?.endsWith(domain))) {
+        if (requestOrigin && !CORS_ALLOWED_DOMAINS.some(domain => requestOrigin.endsWith(domain))) {
             return new Response("Not allowed origin", { 
                 status: 403,
                 headers: {
